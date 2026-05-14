89eb01ca20b2a55c5501b551d3e001e48bb0ceae
Ak Mohd Arief bin Mustapha <akarief@mampunet.btm>
2026-05-14 14:56:41 +0800

    Fix firebase storage bucket URL to use appspot.com

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey:            "AIzaSyAKsrvTMJhoIL86yyUtxyHAVO6ePwOAB9M",
   authDomain:        "catcat-f5c20.firebaseapp.com",
   projectId:         "catcat-f5c20",
-  storageBucket:     "catcat-f5c20.firebasestorage.app",
+  storageBucket:     "catcat-f5c20.appspot.com",
   messagingSenderId: "374151275659",
   appId:             "1:374151275659:web:360c67e17eb5c6ef55a08e"
 };
